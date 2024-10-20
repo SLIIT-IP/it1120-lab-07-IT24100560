9354f89f199b962d2e803846ca9898fcf8524ef2
IT24100560 <it24100560@my.sliit.lk>
2024-10-20 23:38:12 +0530

    Add files via upload

---
+++ Q2/IT24100560lab7Q2B.java
@@ -0,0 +1,19 @@
+public class IT24100560lab7Q2B{
+public static void main(String[] args){
+
+int r,c;
+int k=1;
+
+for(r=1; r<=5; r++){
+	System.out.print(r + "-");
+
+	for(c=1; c<=k; c++){
+		System.out.print("*");
+	}
+	System.out.print("\n");
+	k=k+1;
+
+        }
+}
+
+}
---
+++ Q2/IT24100560lab7Q2C.java
@@ -0,0 +1,16 @@
+public class IT24100560lab7Q2C{
+public static void main(String[] args){
+
+int r,c;
+int k=5;
+
+for(r=1; r<=5; r++){
+
+	for(c=1; c<=k; c++){
+	   System.out.print(k);
+	   }
+	System.out.print("\n");
+	k=k-1;
+	}
+}
+}
